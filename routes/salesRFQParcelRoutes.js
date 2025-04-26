92d7224808c7d817a106ad734154be25633aee71
Shlok <shlok.barot@brainvire.com>
2025-04-26 16:05:33 +0530

    controller updated

--- routes/salesRFQParcelRoutes.js
+++ routes/salesRFQParcelRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const SalesRFQParcelController = require('../controllers/salesRFQParcelController');
-
 const router = express.Router();
+const SalesRFQParcelController1 = require('../controllers/salesRFQParcelController');
 
 // CRUD Routes
-router.get('/', SalesRFQParcelController.getAllSalesRFQParcels);
-router.get('/:id', SalesRFQParcelController.getSalesRFQParcelById);
-router.post('/', SalesRFQParcelController.createSalesRFQParcel);
-router.put('/:id', SalesRFQParcelController.updateSalesRFQParcel);
-router.delete('/:id', SalesRFQParcelController.deleteSalesRFQParcel);
+router.get('/', SalesRFQParcelController1.getAllSalesRFQParcels);
+router.get('/:id', SalesRFQParcelController1.getSalesRFQParcelById);
+router.post('/', SalesRFQParcelController1.createSalesRFQParcel);
+router.put('/:id', SalesRFQParcelController1.updateSalesRFQParcel);
+router.delete('/:id', SalesRFQParcelController1.deleteSalesRFQParcel);
 
 module.exports = router;
